0b1747076ad2fbbf4444e28ec9e6f935767a9b05
akmalfauzi19 <akmal.fauzi19@gmail.com>
2023-06-04 20:10:27 +0700

    fix: cache feed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,5 +1,6 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import User from "@models/user";
 
 export const GET = async (req, res) => {
   try {
@@ -7,9 +8,22 @@ export const GET = async (req, res) => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), {
+    const response = new Response(JSON.stringify(prompts), {
       status: 200,
     });
+
+    // Add a unique identifier to the URL to force a cache-busting reload
+    const url = new URL(req.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+
+    return response;
   } catch (error) {
     return new Response("Failed to fetch all prompts", {
       status: 500,
